314160920f807f321133738066b570c5720e12c7
Edwina Messenger <edwinamessenger@icloud.com>
2022-12-02 00:29:55 +1100

    get pythag numerical value for names

--- js/calc-pythag.js
+++ js/calc-pythag.js
@@ -53,62 +53,119 @@ function stopFlash() {
     clearInterval(timerId);
 }
 
-/////////// OMG FUCK YEAH IT WORKS
-//////just had to put the timerId variable on the settimeout function, NOT the xflash function!
-///// timerId = xflash was just returning the whole function.
-/////timerId = setTimeout... returned the interval id. yay!
-////////weird thing though... I thought that i would not be able to call the timerID variable outside the function
-////OH LOL ACTUALLY JUST GOT IT it's because I initialised it outside the function(top of page)
-/////if it was initialised insde the function, it wouldn't work. let's try...
-//////YES I WAS RIGHT returns an error saying timerId is not defined. yewwww smart bitch
-
-
-
 
 
 /******************************************************************************* 
-******************************  calculate random score *************************
+********  use pythagorean numerology to generate numbers for each name *********
 ********************************************************************************/
 
-var calcBtn = document.querySelector(".calc");
 
-// function getNameOne() {
-//     return(document.querySelector(".name-one").value);
-// }
 
-// calcBtn.addEventListener("click", getNameOne);
+var namee = "ohhhhh weee"; //example name
+var nArray = []; //empty array that will fill with each letter's num value
+var newNum;
+var newArr;
 
-var nOne;
-var nTwo;
-var score = Math.floor(Math.random() * 100);
-var res = document.querySelector(".res");
 
-function giveResult() {
-    calcBtn.addEventListener("click", function() {
-        nOne = document.querySelector("#name-one").value;
-        nTwo = document.querySelector("#name-two").value;
-        console.log(nOne + " and " + nTwo + " have " + score + "% chance of lurrrve");
-        res.classList.remove("res-hid");
-        res.innerHTML = nOne + " and " + nTwo + " have " + score + "% chance of lurrrve";
-    })
+//this function gives the numerical value for each letter and pushes the number onto
+//nArray array
+//letter converter - input letters
+
+function lConv(ll) {
+    if (ll == "a" || ll == "j" || ll == "s") {
+        nArray.push(1);
+    } else if (ll == "b" || ll == "k" || ll == "t") {
+        nArray.push(2);
+    } else if (ll == "c" || ll == "l" || ll == "u") {
+        nArray.push(3);
+    } else if (ll == "d" || ll ==  "m" || ll == "v") {
+        nArray.push(4)
+    } else if (ll == "e" || ll == "n" || ll == "w") {
+        nArray.push(5);
+    } else if (ll == "f" || ll == "o" || ll == "x") {
+        nArray.push(6);
+    } else if (ll == "g" || ll == "p" || ll == "y") {
+        nArray.push(7);
+    } else if (ll == "h" || ll == "q" || ll == "z") {
+        nArray.push(8)
+    } else if (ll == "i" || ll == "r") {
+        nArray.push(9);
+    }
 }
 
-giveResult();
+//this for loop runs through each slice position of the name (i.e. each letter)
+//and runs the letter through lConv function. loop stops at last letter.
+
+function convName(nn){
+    for (let i = 0; i < nn.length; i++){
+        lConv(nn.slice(i,(i + 1)));
+    }
+}
 
 /******************************************************************************* 
-********************************  make result pop up ***************************
+***************************  add up letters of name ****************************
 ********************************************************************************/
 
-//ok so to do this we have to add a new div that is hidden until click (maybe with a timer delay?)
-//and some fun animation of where it comes up from.
-//maybe it's heart shaped and it pops up from the calc btn, hearts floating out around it, and it beats
-//like a heart as it sits there?
-//sit div on top of btn, hidden.  
+
+//so you have an array. you add all the numbers of the array and get a new number.
+//you turn the number into a string. turn this into an array.
+//add up the array. get a new number. turn into string. turn this into an array.
+//add up the array. get a new number. turn into string. turn this into an array.
+//etc. until the number is below 10. if the number is not below 10, it gets turned into a string/ array.
 
 
 
-/******************************************************************************* 
-********************************  make restart button **************************
-********************************************************************************/
 
-//to do this do i maybe need to put everything in a loadpage function and then on click restart run function again?
+//this function adds all numbers in a given array and adds the value to the newNum var. returns newNum.
+function add(arr) {
+    newNum = 0
+    for (i = 0; i < arr.length; i++) {
+        newNum += arr[i];
+    }
+    console.log(newNum);
+    return(newNum);
+}
+
+//this function turns number into array. function works.
+function toArray(num) {
+    newArr = []
+    num = num.toString();
+    for (i = 0; i < num.length; i++) {
+        newArr.push(num.slice(i, i + 1));
+    }
+    console.log(newArr);
+    return(newArr);
+}
+
+//this function is used when the number is down to 2 digits.
+//turns both items in array into integers, adds them, returns a number (newNum).
+function addTwo(arr){
+    newNum = parseInt(arr[0]) + parseInt(arr[1]);
+    return(newNum);
+}
+
+
+//doIt is to calculate a given name's number. pass in the name.
+function doIt(thing) {
+    add(thing); //adds up items in array. returns number.
+
+    if (newNum < 10) { //check number
+        console.log("final number is " + newNum);
+        return(newNum); //if number under 10, return.
+    } else {
+        toArray(newNum); //if number over 10, pass number through toArray function. returns an array (newArr)
+        addTwo(newArr); //pass newArr through addTwo fn. this adds 2 items and returns a number. //returns 11
+        if (newNum < 10) {
+            console.log("final number is " + newNum);
+            return(newNum);
+        } else {
+            toArray(newNum);
+            addTwo(newArr);
+            console.log("final number is " + newNum);
+            return(newNum);
+        }
+     }
+}
+
+convName(namee);
+doIt(nArray);
